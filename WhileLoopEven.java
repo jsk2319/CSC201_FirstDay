85fdf518fe4db09711b26dabfcb6f8b0270da8c4
jsk2319 <31487832+jsk2319@users.noreply.github.com>
2018-01-14 13:24:47 -0500

    Update WhileLoopEven.java

--- WhileLoopEven.java
+++ WhileLoopEven.java
@@ -1,14 +1,12 @@
 public class WhileLoopEven {
     public static void main(String[] args) {
-        int limit = 100;
-        int i = 2;
+        int e = 2;
 
-        while (i<limit) {
-            System.out.println(i + 2 + " ");
-            int even = i + 2;
-            if ( i > limit) {
-                break;
-            }
+        while (e <= 100) {
+
+            System.out.print(e + " ");
+
+            e += 2;
         }
 
     }
